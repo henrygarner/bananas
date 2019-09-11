a3624b00473013e136598fe9870468cc5d6096b2
Henry Garner <henry@henrygarner.com>
2019-09-11 23:25:57 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject bananas "0.0.1-SNAPSHOT"
+  :description "Recursion schemes"
+  :url "https://github.com/henrygarner/bananas"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[funcool/cats "2.3.2"]]
+  :dev-dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns bananas.core})
---
+++ src/bananas/core.clj
@@ -0,0 +1,52 @@
+(ns bananas.core
+  (:require [cats.core :as m]
+            [cats.protocols :as p]
+            [cats.monad.either :as either]
+            [cats.builtin]))
+
+(defmacro <<< [& args]
+  `(fn [x#]
+     ((comp ~@args) x#)))
+
+(defmacro >>> [& args]
+  `(fn [x#]
+     ((comp ~@(reverse args)) x#)))
+
+(defn &&&
+  [f g]
+  (fn [x]
+    [(f x) (g x)]))
+
+(defn |||
+  [lf rf]
+  (fn [x]
+    (either/branch x lf rf)))
+
+(defn functor?
+  [v]
+  (and (satisfies? p/Contextual v)
+       (satisfies? p/Functor (p/-get-context v))))
+
+(defn fmap*
+  [f v]
+  (cond->> v
+    (functor? v)
+    (m/fmap f)))
+
+(def fmap (m/curry fmap*))
+
+(defn cata
+  [f]
+  (>>> (fmap (cata f)) f))
+
+(defn ana
+  [f]
+  (<<< (fmap (ana f)) f))
+
+(defn para
+  [f]
+  (>>> (&&& identity (fmap (para f))) f))
+
+(defn apo
+  [f]
+  (<<< (||| identity (fmap (apo f))) f))
