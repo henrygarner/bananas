3fd498c5140e49990fd60f086ef7d1ca9f9e7afb
Henry Garner <henry@henrygarner.com>
2019-09-14 20:23:24 +0100

    Add ana tests

--- test/bananas/core_test.clj
+++ test/bananas/core_test.clj
@@ -1,6 +1,7 @@
 (ns bananas.core-test
   (:require [clojure.test :refer [deftest is]]
-            [bananas.core :as sut]))
+            [bananas.core :as sut]
+            [clojure.string :as str]))
 
 (defn cata-helper
   [arg]
@@ -15,3 +16,18 @@
     (is (= 9 (f [[1] [2] [3]])))
     (is (= 9 (f [[1 [2 [3]]]])))
     (is (= 9 (f #{1 2 3})))))
+
+(defn ana-helper
+  [n]
+  (if (pos? n)
+    [(dec n)]
+    n))
+
+(deftest ana
+  []
+  (let [f (sut/ana ana-helper)]
+    (is (= [0] (f 1)))
+    (is (= [[0]] (f 2)))
+    (is (= [[[0]]] (f 3)))
+    (is (= [[[[0]]]] (f 4)))))
+
